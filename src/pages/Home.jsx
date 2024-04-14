e049243d7dd66d486c3a40f13746fc45f9cbe04d
miss-gif <ddc.akari@gmail.com>
2024-04-14 17:00:51 +0900

    Design : page Home 작업

---
+++ src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import PangImage from "../assets/ggompang.jpeg";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Contents>
+        <Title>나에게 맞는 주인님은?</Title>
+        <LogoImage>
+          <img alt="고양이 사진" src={PangImage} />
+        </LogoImage>
+        <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
+        <button>테스트 시작하기</button>
+      </Contents>
+    </Wrapper>
+  );
+};
+
+export default Home;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "SimKyungha";
+`;
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+  font-family: "SimKyungha";
+`;
+
+const LogoImage = styled.div`
+  margin-top: 10px;
+`;
+
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+  font-family: "SimKyungha";
+`;
