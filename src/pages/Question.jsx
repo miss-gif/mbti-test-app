2a5c1f2399277e43522538c78a159711bdad1908
miss-gif <ddc.akari@gmail.com>
2024-04-14 23:43:29 +0900

    Design : page Question 작업

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import styled from "styled-components";
+import { QuestionData } from "../assets/data/questiondata";
 
 const Question = () => {
-  return <div>Question</div>;
+  return (
+    <Wrapper>
+      <Title>{QuestionData[0].title}</Title>
+      <button>{QuestionData[0].answera}</button>
+      <button>{QuestionData[0].answerb}</button>
+    </Wrapper>
+  );
 };
 
 export default Question;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+`;
